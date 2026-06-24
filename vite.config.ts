f01bbeda8364f8f086bf92342362825747a8ba9f
MathyasMartins <mathyasmartins25@gmail.com>
2026-06-24 16:45:14 -0300

    atualização v1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,53 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
-import path from 'path'
+
+import path from 'node:path'
 
 export default defineConfig({
-  plugins: [react()],
+
+  plugins: [
+    react()
+  ],
+
   resolve: {
+
     alias: {
+
       '@': path.resolve(__dirname, './src'),
+
       '@components': path.resolve(__dirname, './src/components'),
+
       '@services': path.resolve(__dirname, './src/services'),
+
       '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@types': path.resolve(__dirname, './src/types'),
+
+      '@app-types': path.resolve(__dirname, './src/types'),
+
       '@utils': path.resolve(__dirname, './src/utils'),
+
       '@domain': path.resolve(__dirname, './src/domain'),
+
       '@core': path.resolve(__dirname, './src/core'),
+
       '@adapters': path.resolve(__dirname, './src/adapters'),
-      '@features': path.resolve(__dirname, './src/features'),
-    },
+
+      '@features': path.resolve(__dirname, './src/features')
+
+    }
+
   },
+
   build: {
+
     target: 'ES2020',
-    minify: 'terser',
+
+    minify: 'esbuild',
+
     sourcemap: false,
-    outDir: 'dist',
-  },
+
+    outDir: 'dist'
+
+  }
+
 })
