f52bf28a79050829e2a4995d7f973642b4d6ff35
MathyasMartins <mathyasmartins25@gmail.com>
2026-07-25 11:49:24 -0300

    teste correção rota MenuPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,20 @@
-// Aplicação Principal
-
-import React from 'react'
+// App.tsx
+import React, { useState } from 'react'
 import { GamePage } from '@/pages/GamePage'
+import { MenuPage } from '@/pages/MenuPage'
 import '@/styles/globals.css'
 
 export const App: React.FC = () => {
+  // Estado para alternar entre 'menu' e 'game'
+  const [currentPage, setCurrentPage] = useState<'menu' | 'game'>('menu')
+
   return (
     <div className="min-h-screen bg-dark text-light">
-      <GamePage />
+      {currentPage === 'menu' ? (
+        <MenuPage onStartGame={() => setCurrentPage('game')} />
+      ) : (
+        <GamePage />
+      )}
     </div>
   )
 }
